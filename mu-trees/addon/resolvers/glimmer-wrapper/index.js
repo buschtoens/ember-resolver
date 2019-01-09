5d48bb095017f3ec6887d8a450ca7b76e9378e6c
Jan Buschtöns <jan@buschtoens.me>
2019-01-09 14:39:55 +0100

    fix(glimmer-wrapper): move debug `normalize` in `.extend()` block

--- mu-trees/addon/resolvers/glimmer-wrapper/index.js
+++ mu-trees/addon/resolvers/glimmer-wrapper/index.js
@@ -85,7 +85,15 @@ const Resolver = GlobalsResolver.extend({
     this._glimmerResolver = new GlimmerResolver(this.config, this.glimmerModuleRegistry);
   },
 
-  normalize: null,
+  normalize: !DEBUG ? null : function(specifier) {
+    // This method is called by `Registry#validateInjections` in dev mode.
+    // https://github.com/ember-cli/ember-resolver/issues/299
+    const [type, name] = specifier.split(':', 2);
+    if (name && (type === 'service' || type === 'controller')) {
+      return `${type}:${dasherize(name)}`;
+    }
+    return specifier;
+  },
 
   expandLocalLookup(specifier, source, namespace) {
     if (isAbsoluteSpecifier(specifier)) {
@@ -150,17 +158,5 @@ const Resolver = GlobalsResolver.extend({
   }
 
 });
-
-if (DEBUG) {
-  Resolver.prototype.normalize = function(specifier) {
-    // This method is called by `Registry#validateInjections` in dev mode.
-    // https://github.com/ember-cli/ember-resolver/issues/299
-    const [type, name] = specifier.split(':', 2);
-    if (name && (type === 'service' || type === 'controller')) {
-      return `${type}:${dasherize(name)}`;
-    }
-    return specifier;
-  }
-}
 
 export default Resolver;
